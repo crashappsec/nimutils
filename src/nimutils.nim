7dc948da23df2e713d2cdae2ab5f14f815b6c87d
John Viega <john@zork.org>
2023-01-11 23:46:31 -0500

    Import texttable automatically w/ the package

--- src/nimutils.nim
+++ src/nimutils.nim
@@ -1,2 +1,2 @@
-import nimutils/[box, random, unicodeid, topics, sinks, misc, ansi]
-export box, random, unicodeid, topics, sinks, misc, random, ansi
+import nimutils/[box, random, unicodeid, topics, sinks, misc, ansi, texttable]
+export box, random, unicodeid, topics, sinks, misc, random, ansi, texttable
