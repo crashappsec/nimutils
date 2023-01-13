a9311fb5674b49aee6e9414de8c8dd0a1a84c234
John Viega <john@zork.org>
2023-01-13 16:45:54 -0500

    Added a \'File Table\', which, at compile time, creates a table object that you can make const, reading the values from the contents of files in a directory, and the keys from the file names (with any extension chopped off).  I intend to use this for easily managing help content and potentially configuration files.

--- src/nimutils.nim
+++ src/nimutils.nim
@@ -1,2 +1,4 @@
-import nimutils/[box, random, unicodeid, topics, sinks, misc, ansi, texttable]
-export box, random, unicodeid, topics, sinks, misc, random, ansi, texttable
+import nimutils/[box, random, unicodeid, topics, sinks, misc, ansi, texttable],
+       nimutils/filetable
+export box, random, unicodeid, topics, sinks, misc, random, ansi, texttable,
+       filetable
---
+++ src/nimutils/filetable.nim
@@ -0,0 +1,54 @@
+## The basic idea here is to create const tables that are populated
+## from a directory on the file system.  This only looks at flat
+## files, and does no recursion.
+##
+## The keys are taken from the file name, with the extension, if any,
+## ripped off of it.
+import tables, strutils, os
+
+type
+  FileTable        = Table[string, string]
+  OrderedFileTable = OrderedTable[string, string]
+
+
+proc staticListFiles*(arg: string): seq[string] =
+  # Unfortunately, for whatever reason, system/nimutils doesn't seem
+  # to work from here, so we can't use listFiles().  As a result, we
+  # use a staticexec("ls") and parse.  This obviously is not portable
+  # to all versions of Windows.
+  #
+  # This invocation of ls might not be super portable.  Deserves a bit
+  # of testing.
+  result = @[]
+
+  let
+    lines = staticExec("ls -mp " & arg)
+    line  = lines.replace("\n", " ")
+    
+  return split(line, ", ")
+
+template newFileTableBase(typeName: typeDesc, dir: static[string]): FileTable =
+  var ret: typeName[string, string] = `init typeName`[string, string]()
+  
+  for filename in staticListFiles(`dir`):
+    if filename.endswith("/"): continue
+    let
+      fileContents = staticRead(dir.joinPath(filename))
+      key          = splitFile(filename).name
+        
+    ret[key] = fileContents
+    
+  ret
+
+proc newFileTable*(dir: static[string]): FileTable =
+  return newFileTableBase(Table, dir)
+
+proc newOrderedFileTable*(dir: static[string]): OrderedFileTable =
+  return  newFileTableBase(OrderedTable, dir)
+    
+when isMainModule:
+  const x = newFileTable("/Users/viega/dev/sami/src/help/")
+
+  for k, v in x:
+    echo "Filename: ", k
+    echo "Contents: ", v[0 .. 40], "..."
