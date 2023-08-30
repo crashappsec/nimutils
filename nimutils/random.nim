464233d5a59d607e2f368d36216ed02a18a93f4f
John Viega <john@zork.org>
2023-08-30 01:05:29 -0400

    Add a method that gives a random string. Ran into a nim 2 bug and ended up using openssl's API instead.

--- nimutils/random.nim
+++ nimutils/random.nim
@@ -1,7 +1,7 @@
 ## :Author: John Viega (john@crashoverride.com)
 ## :Copyright: 2022, 2023, Crash Override, Inc.
 
-import std/sysrand
+import std/sysrand, openssl, strutils
 
 template secureRand*[T](): T =
   ## Returns a uniformly distributed random value of any _sized_ type without
@@ -15,8 +15,35 @@ template secureRand*[T](): T =
 proc randInt*(): int =
   return secureRand[int] and (not (1 shl 63))
 
+# I'm hitting what seems to be a nim 2.0 bug w/ urandom() to a
+# dynamically alloc'd value.
+# The openSSL PRNG is good for this case.
+
+proc RAND_bytes(p: pointer, i: cint): cint {.cdecl, dynlib: DLLUtilName,
+                                              importc.}
+
+proc bytesToString*(b: ptr UncheckedArray[char], l: int): string =
+  for i in 0 ..< l:
+    result.add(b[i])
+
+template bytesToString*(b: ptr char, l: int): string =
+  bytesToString(cast[ptr UncheckedArray[char]](b), l)
+
+proc randString*(l: int): string =
+  ## Get a random binary string of a particular length.
+  var b = cast[ptr char](alloc(l))
+
+  discard RAND_bytes(b, cint(l))
+  result = bytesToString(cast[ptr UncheckedArray[char]](b), l)
+  dealloc(b)
+
+template randStringHex*(l: int): string =
+  randString(l).toHex().toLowerAscii()
+
 when isMainModule:
-    echo secureRand[uint64]()
-    echo secureRand[int32]()
-    echo secureRand[float]()
-    echo secureRand[array[6, byte]]()
+  import strutils
+  echo secureRand[uint64]()
+  echo secureRand[int32]()
+  echo secureRand[float]()
+  echo secureRand[array[6, byte]]()
+  echo randString(12).toHex()
