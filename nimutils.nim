6e17663ebf260fa9279bc94dba3f45ad027746f7
John Viega <john@zork.org>
2023-08-30 17:54:10 -0400

    Also add hexdump.

--- nimutils.nim
+++ nimutils.nim
@@ -20,10 +20,10 @@
 
 import nimutils/[box, random, unicodeid, pubsub, sinks, misc, ansi, texttable],
        nimutils/[file, process, filetable, encodings, advisory_lock, formatstr]
-import nimutils/[sha, aes, prp]
+import nimutils/[sha, aes, prp, hexdump]
 export box, random, unicodeid, pubsub, sinks, misc, random, ansi, texttable,
        file, process, filetable, encodings, advisory_lock, formatstr,
-       sha, aes, prp
+       sha, aes, prp, hexdump
 
 ## Things we don't want to force people to consume need to be imported
 ## manually. Currently, that's:
