063f5f6e41ec99e324c3c2ce58e260b756a6c052
John Viega <john@zork.org>
2023-11-13 00:36:38 -0500

    Logging API accepts ropes now

--- nimutils/logging.nim
+++ nimutils/logging.nim
@@ -2,7 +2,7 @@
 ## :Copyright: 2023, Crash Override, Inc.
 
 import tables, options, streams, pubsub, sinks, strutils,
-       rope_base, rope_construct, rope_ansirender,
+       rope_base, rope_construct, rope_ansirender
 
 type LogLevel* = enum
   ## LogLevel describes what kind of messages you want to see.
