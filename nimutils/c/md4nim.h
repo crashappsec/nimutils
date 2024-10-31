b21027354e70cdb4490aa2fb813727aea7bd1383
Miroslav Shubernetskiy <miroslav@miki725.com>
2024-10-31 11:36:56 -0400

    removing md4c.h in favor of n00b.h (#79)
    * removing md4c.h in favor of n00b.h
    
    for now the header is not included directly but is meant to be already
    present on the system via other means
    
    also adjusting crownhash implementation to be n00b hatrack compatible
    
    * ignoring tests on libn00b branch

---
+++ nimutils/c/md4nim.h
@@ -0,0 +1,26 @@
+#define HATRACK_PER_INSTANCE_AUX
+#include <n00b.h>
+
+// John's stuff.
+typedef void (*CB_TYPE)(const char *, unsigned int, void*);
+
+typedef struct {
+    CB_TYPE process_output;
+    void *unused1;
+    void *unused2;
+    void *unused3;
+} MD_HTML_CALLBACKS;
+
+N_LIB_PRIVATE N_CDECL(void, nimu_process_markdown)(NU8* s_p0, unsigned int n_p1, void* p_p2);
+
+MD_HTML_CALLBACKS x = {
+  .process_output   = nimu_process_markdown,
+  .unused1 = NULL,
+  .unused2 = NULL,
+  .unused3 = NULL
+};
+
+int
+c_markdown_to_html(char *mdoc, size_t len, void *outobj, size_t flags) {
+  return md_html(mdoc, len, x, outobj, flags & 0x1ffff, flags >> 28);
+}
