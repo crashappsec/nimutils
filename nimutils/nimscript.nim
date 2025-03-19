0ba224b70543a9609cefc511d311d9397099f2cf
Miroslav Shubernetskiy <miroslav@miki725.com>
2025-03-19 09:56:29 -0400

    md n00b header var

--- nimutils/nimscript.nim
+++ nimutils/nimscript.nim
@@ -75,6 +75,9 @@ template applyCommonLinkOptions*(staticLink = true, quiet = true) =
   switch("passC", "-Wno-error=implicit-function-declaration")
   switch("passC", "-Wno-error=incompatible-pointer-types")
 
+  # n00b
+  switch("passC", "-DHATRACK_PER_INSTANCE_AUX")
+
   setupTargetArch(quiet)
 
   when defined(macosx):
